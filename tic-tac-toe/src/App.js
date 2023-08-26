27ef9d6c19742afe71e4368923982035ec20a98c
ke-stack <kingkevo254@gmail.com>
2023-08-26 23:14:54 +0300

    Added a celebration animation and added the logic to display once a winner was found

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -32,39 +32,56 @@ export default function Board() {
 
   if (winner) {
     status = "Winner: " + winner;
+    let celebration = document.getElementsByClassName('celebration');
+    celebration.style.display = 'true';
+    
   } else {
     status = "Next player: " + (xIsNext ? "X" : "O");
   }
 
 
 
+
   return (
     <>
     
-    <div className="board">
-    <div className='status'>
-      {status}
-    </div>
+      <div className="board">
 
-      <div className='board-row'>
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
-      </div>
+      <div className='celebration'>
+            <lord-icon
+                src="https://cdn.lordicon.com/ihyatngg.json"
+                trigger="hover"
+                colors="primary:#4be1ec,secondary:#cb5eee"
+                className='lordicon'>
+            </lord-icon>
+        </div> 
 
-      <div className='board-row'>
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
-      </div>
+        <div className='status'>
+          {status}
+        </div>       
 
-      <div className='board-row'>
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
+        <div>
+
+          <div className='board-row'>
+            <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
+            <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
+            <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
+          </div>
+
+          <div className='board-row'>
+            <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
+            <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
+            <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
+          </div>
+
+          <div className='board-row'>
+            <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
+            <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
+            <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
+          </div>
+
+        </div>    
       </div>
-          
-    </div>
 
     </>
   );
